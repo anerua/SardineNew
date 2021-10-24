eb80e2cc4e84147f39ebf46148560ab6dd02da67
Martins Anerua <aneruamartins@gmail.com>
2021-10-24 09:30:14 +0100

    modifies project root in settings.py

--- Sardine/settings.py
+++ Sardine/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+# PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra lookup directories for collectstatic to find static files
